5847a86682bff6d1ac2665c75105511c867f508a
BobSomething <61118026+BobSomething@users.noreply.github.com>
2022-10-05 09:50:40 +0200

    Add files via upload

---
+++ compile/zad1/lev.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+
+#include "lev.h"
+
+using namespace std;
+
+#define tail(a) string(a,1)
+
+unsigned lev(string a, string b)
+{
+	if(a.size() == 0) return a.size();
+	if(b.size() == 0) return b.size();
+	if(a[0] == b[0]) return lev(tail(a), tail(b));
+	return min({lev(tail(a),b), lev(a,tail(b)), lev(tail(a), tail(b))}) + 1;
+}
+
---
+++ compile/zad1/lev.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include<string>
+
+unsigned lev(std::string, std::string);
